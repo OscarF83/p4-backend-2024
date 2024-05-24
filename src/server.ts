632dac47528a682b023f005a295f25fe85d36ccb
Oscar <o_fonte@hotmail.com>
2024-05-24 17:56:23 +0200

    Mi primer endpoint /technicians

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { db } from './db';
 
 const app = express();
 
@@ -8,9 +9,16 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-    res.status(200).json({ok: true, message: "Hello"});
-})
+app.get("/technicians", async (req, res) => {
+    try{
+        const technicians = await db.technicians.findMany({
+        orderBy:{firstName: "asc"}
+        })
+        res.status(200).json(technicians);
+    }catch (e){
+        res.status(500).json({error: "Internal Error"});
+    }
+});
 
 const {PORT} = process.env;
 app.listen(PORT, () => {
